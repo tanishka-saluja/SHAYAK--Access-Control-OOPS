b017ae43a9c9d895d5e9ed2c745bf1b595c20b08
Tanishka saluja <tanishka.saluja07@gmail.com>
2026-02-11 10:51:16 +0530

    Update WomanEntrepreneur.java

--- src/WomanEntrepreneur.java
+++ src/WomanEntrepreneur.java
@@ -1,11 +1,13 @@
 class WomanEntrepreneur extends User {
     private double income;
     private int businessYears;
+    private String educationLevel; 
 
-    public WomanEntrepreneur(String id, double income, int years) {
+    public WomanEntrepreneur(String id, double income, int years, String educationLevel) {
         super(id, "APPLICANT");
         this.income = income;
         this.businessYears = years;
+        this.educationLevel = educationLevel;
     }
 
     public double getIncome() {
@@ -14,5 +16,9 @@ class WomanEntrepreneur extends User {
 
     public int getBusinessYears() {
         return businessYears;
+    }
+
+    public String getEducationLevel() {
+        return educationLevel;
     }
 }
