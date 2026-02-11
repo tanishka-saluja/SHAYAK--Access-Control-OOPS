f4b2d9e48e47768b1dcd07c2a8e178853923e296
Tanishka saluja <tanishka.saluja07@gmail.com>
2026-02-11 11:38:16 +0530

    Update CreditScorer.java

--- src/CreditScorer.java
+++ src/CreditScorer.java
@@ -1,12 +1,36 @@
-public class EducationPolicy {
+public class CreditScorer {
 
-    public static int getEducationPoints(String education) {
+    public int calculateScore(WomanEntrepreneur user) {
 
-        switch (education.toLowerCase()) {
-            case "postgraduate": return 30;
-            case "graduate": return 20;
-            case "12th": return 10;
-            default: return 5;
-        }
+        int score = 0;
+
+        if (user.getMonthlyIncome() >= 15000)
+            score += 30;
+        else if (user.getMonthlyIncome() >= 10000)
+            score += 25;
+        else
+            score += 15;
+
+        if (user.getYearsInBusiness() >= 5)
+            score += 25;
+        else if (user.getYearsInBusiness() >= 3)
+            score += 20;
+        else
+            score += 10;
+
+        score += EducationPolicy.getEducationPoints(user.getEducation());
+
+        return score;
+    }
+
+    public void explainScore(WomanEntrepreneur user) {
+
+        int score = calculateScore(user);
+
+        System.out.println("---- Credit Score Breakdown ----");
+        System.out.println("Income: " + user.getMonthlyIncome());
+        System.out.println("Years in Business: " + user.getYearsInBusiness());
+        System.out.println("Education: " + user.getEducation());
+        System.out.println("Final Credit Score: " + score);
     }
 }
