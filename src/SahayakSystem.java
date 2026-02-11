9367bd0b795935e30c3f8e048bcd20fe91a3526d
Tanishka saluja <tanishka.saluja07@gmail.com>
2026-02-11 11:43:36 +0530

    Update SahayakSystem.java

--- src/SahayakSystem.java
+++ src/SahayakSystem.java
@@ -2,13 +2,28 @@ public class SahayakSystem {
 
     public static void main(String[] args) {
 
+        LanguageSupport.displayWelcome("EN");
+
         WomanEntrepreneur lakshmi =
-            new WomanEntrepreneur("L001", 12000, 3, "Graduate");
+                new WomanEntrepreneur("L001",
+                        "Lakshmi",
+                        12000,
+                        4,
+                        "Graduate");
 
         Loan loan = new Loan();
 
+        LoanPolicy.displayLoanTerms(50000, 12);
+
         Verifier officer = new BankVerifier("B101");
+
         AccessController.processLoan(officer, lakshmi, loan);
+
+        Grievance grievance =
+                new Grievance("Need EMI clarification.");
+        grievance.register();
+
+        DigitalLedger.showRecords();
 
         System.out.println("Final Loan Status: " + loan.getStatus());
     }
