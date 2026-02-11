f1907c70c76e98d0e7ba626b8008e6d7b902484a
Tanishka saluja <tanishka.saluja07@gmail.com>
2026-02-11 10:54:11 +0530

    Update AccessController.java

--- src/AccessController.java
+++ src/AccessController.java
@@ -1,19 +1,30 @@
-class AccessController {
+class CreditScorer {
 
-    public static void processLoan(Verifier verifier, Loan loan, boolean approve) {
+    public static int calculateScore(WomanEntrepreneur w) {
+        int score = 0;
 
-        if (!verifier.isAuthorized()) {
-            AuditLogger.log(verifier.userId, "UNAUTHORIZED ACCESS ATTEMPT");
-            System.out.println("Access Denied ");
-            return;
+        // Base financial criteria
+        if (w.getIncome() >= 10000) score += 30;
+        if (w.getBusinessYears() >= 2) score += 20;
+
+        // Education-based preference (transparent)
+        switch (w.getEducationLevel().toLowerCase()) {
+            case "postgraduate":
+                score += 25;
+                break;
+            case "graduate":
+                score += 20;
+                break;
+            case "diploma":
+                score += 15;
+                break;
+            case "school":
+                score += 10;
+                break;
+            default:
+                score += 5;
         }
 
-        if (approve) {
-            loan.approve();
-            AuditLogger.log(verifier.userId, "Loan Approved");
-        } else {
-            loan.reject();
-            AuditLogger.log(verifier.userId, "Loan Rejected");
-        }
+        return score;
     }
 }
